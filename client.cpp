6fa5cfa2183cb7b559fdd69caf2747d09d36f68e
Owen1Campbell <ocampbell@knights.ucf.edu>
2022-10-06 16:10:02 -0400

    calc features functional

--- client.cpp
+++ client.cpp
@@ -22,10 +22,52 @@ bool inputIsValid(string in) {
             return false;
     }
 
+    // invalid if missing operator or does not end in '='
     if (in[in.length()-1] != '=' || !hasOp)
         return false;
 
+    // check if expression has no second term (ex: 1+=)
+    if (in[in.length()-2] == '+' || in[in.length()-2] == '-' || in[in.length()-2] == '*' || in[in.length()-2] == '/') 
+        return false;
     return true;
+}
+
+float execute(string exp) {
+    string pt1, pt2;
+    char op;
+    float term1, term2;
+
+    int i = 0;
+    while (exp[i] != '+' && exp[i] != '-' && exp[i] != '*' && exp[i] != '/') {
+        pt1 += exp[i];
+        i++;
+    }
+
+    term1 = stof(pt1);
+    op = exp[i];
+    i++;
+
+    while (exp[i] != '=') {
+        pt2 += exp[i];
+        i++;
+    }
+
+    term2 = stof(pt2);
+
+    switch (op) {
+        case '+':
+            return term1 + term2;
+        case '-':
+            return term1 - term2;
+        case '*':
+            return term1 * term2;
+        case '/':
+            return term1 / term2;
+        default:
+            cout << "math error :(" << endl;
+    }
+
+    return 0;
 }
 
 int main() {
@@ -35,7 +77,8 @@ int main() {
     while(input != "0/0=") 
     {
         if (inputIsValid(input)) {
-            cout << "valid";
+            cout << "valid" << endl;
+            cout << "result is " << execute(input) << endl;
         }
         else
             cout << "Input error. Re-type the math question again." << endl;
